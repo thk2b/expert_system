74bb0f4d9637daf976b107cfeca2e7bf0f575e05
thk2b <theokobb@gmail.com>
2019-02-25 17:53:38 -0800

    write tests

--- KnowledgeBase.py
+++ KnowledgeBase.py
@@ -4,7 +4,7 @@ class KnowledgeBase:
     def __init__(self, rules={}, facts=[]):
         """
         Knowledge Base
-            rules: dict of Expr(antecedent): Expr(concequent).
+            rules: dict of Expr(antecedent): [Expr(concequent)].
                 If the antecedent is true, the concequent must be true
             facts: list of Expressions known to be true
         """
@@ -12,7 +12,7 @@ class KnowledgeBase:
         self.facts = facts
 
     def _is_fact(self, expr):
-        for fact in facts:
+        for fact in self.facts:
             if isinstance(fact, Atom) and isinstance(expr, Atom):
                 return fact.symbol == expr.symbol
         return False
@@ -22,30 +22,19 @@ class KnowledgeBase:
             if verbose:
                 print("{} is True.".format(expr))
             return True
-        for antecedent, concequent in self.rules.items():
-            if expr in concequent and self.query(antecedent, verbose):
-                
-                # concequent may entail expr and antecedent is true
-                # if concequent.evaluate(expr, self.facts):
-                    # facts and concequent entail expression
-                if verbose:
-                    print("Therefore {} is True.".format(expr))
-                return True
+        for antecedent, concequents in self.rules.items():
+            for concequent in concequents:
+                if expr in concequent and self.query(antecedent, verbose):
+                    # concequent may entail expr and antecedent is true
+                    # if concequent.evaluate(expr, self.facts):
+                        # facts and concequent entail expression
+                    if verbose:
+                        print("Therefore {} is True.".format(expr))
+                    return True
         return False
-    
+
     def add_fact(self, expr):
         self.facts.append(expr)
 
     def add_rule(self, antecedent, concequent):
         self.rules[antecedent] = concequent
-
-if __name__ == "__main__":
-    rules = {
-        Atom('A'): Atom('B'),
-        Atom('B'): Atom('C')
-    }
-    facts = [
-        Atom('A')
-    ]
-    kb = KnowledgeBase(rules, facts)
-    kb.query(Atom('C'), verbose=True)
---
+++ test.py
@@ -0,0 +1,87 @@
+from KnowledgeBase import KnowledgeBase
+from expressions import Atom, AndExpression
+import unittest
+
+class TestBasicKnowledgeBase(unittest.TestCase):
+    def test_simple_backwards_chaining(self):
+        """A -> B, X -> D, B -> C, =A, ?ACD"""
+        rules = {
+            Atom('A'): [Atom('B')],
+            Atom('X'): [Atom('D')],
+            Atom('B'): [Atom('C')]
+        }
+        facts = [
+            Atom('A')
+        ]
+        kb = KnowledgeBase(rules, facts)
+        self.assertTrue(kb.query(Atom('A')))
+        self.assertTrue(kb.query(Atom('C')))
+        self.assertFalse(kb.query(Atom('D')))
+
+    def test_multiple_rules_same_antecedent(self):
+        """A -> B, A -> C, =A, ?ABC"""
+        rules = {
+            Atom('A'): [Atom('B')],
+            Atom('A'): [Atom('C')],
+        }
+        facts = [
+            Atom('A')
+        ]
+        kb = KnowledgeBase(rules, facts)
+        self.assertTrue(kb.query(Atom('A')))
+        self.assertTrue(kb.query(Atom('B')))
+        self.assertTrue(kb.query(Atom('C')))
+
+    def test_not_expression_as_antecedent(self):
+        """!A -> B, =, ?AB"""
+        rules = {
+            NotExpression(Atom('A')): [Atom('B')]
+        }
+        facts = []
+        kb = KnowledgeBase(rules, facts)
+        self.assertFalse(kb.query(Atom('A')))
+        self.assertTrue(kb.query(Atom('B')))
+
+    def test_and_expression_as_antecedent(self):
+        """A + B -> C, A -> B, =A, ?ABC"""
+        rules = {
+            AndExpression(Atom('A'), Atom('B')): [Atom('C')],
+            Atom('A'): [Atom('B')]
+        }
+        facts = [
+            Atom('A')
+        ]
+        kb = KnowledgeBase(rules, facts)
+        self.assertTrue(kb.query(Atom('A')))
+        self.assertTrue(kb.query(Atom('B')))
+        self.assertTrue(kb.query(Atom('C')))
+
+    def test_or_expression_as_concequent(self):
+        """A -> B | C, A -> !B, =A, ?ABC"""
+        rules = {
+            Atom('A'): [OrExpression(Atom('B'), Atom('C'))],
+            Atom('A'): [NotExpression(Atom('B'))]
+        }
+        facts = [
+            Atom('A')
+        ]
+        kb = KnowledgeBase(rules, facts)
+        self.assertTrue(kb.query(Atom('A')))
+        self.assertFalse(kb.query(Atom('B')))
+        self.assertTrue(kb.query(Atom('C')))
+
+    def test_and_expression_as_concequent(self):
+        """A -> B + C, =A, ?ABC"""
+        rules = {
+            Atom('A'): [AndExpression(Atom('B'), Atom('C'))],
+        }
+        facts = [
+            Atom('A')
+        ]
+        kb = KnowledgeBase(rules, facts)
+        self.assertTrue(kb.query(Atom('A')))
+        self.assertTrue(kb.query(Atom('B')))
+        self.assertTrue(kb.query(Atom('C')))
+
+if __name__ == '__main__':
+    unittest.main()
